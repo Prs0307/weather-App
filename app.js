fef9a037d3d9a981c25e3e9b08adf75d85eda1bc
Prince Sahu <116274467+SSAHHC@users.noreply.github.com>
2023-08-08 17:09:23 +0530

    added styles

--- app.js
+++ app.js
@@ -1,49 +1,51 @@
-const express = require('express');
-const https = require('https');
+const express = require("express");
+const https = require("https");
 const app = express();
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-const bodyParser = require('body-parser');
-const { error } = require('console');
+const bodyParser = require("body-parser");
+const { error } = require("console");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  console.log("get method called");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post('/', (req, res) => {
+app.post("/", (req, res) => {
+  console.log("post method called");
+
   const CitySearched = req.body.cityName;
   const url_api =
-    'https://api.openweathermap.org/data/2.5/weather?q=' +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     CitySearched +
-    '&appid=bbde3cc20f37d6c576ef76bf7ae7539b';
+    "&appid=bbde3cc20f37d6c576ef76bf7ae7539b";
 
   https.get(url_api, (response) => {
-    response.on('data', (data) => {
+    response.on("data", (data) => {
       const weatherData = JSON.parse(data);
       const temp = (weatherData.main.temp - 273.15).toFixed(2);
       const weatherDescription = weatherData.weather[0].description;
       const Humidity = weatherData.main.humidity;
       const windSpeed = weatherData.wind.speed;
-      const countryName=weatherData.sys.country;
-//       var weatherImg= weatherData.weather[0].description;
+      const countryName = weatherData.sys.country;
+      //       var weatherImg= weatherData.weather[0].description;
 
-//       const bodyElement = document.getElementById("body");
+      //       const bodyElement = document.getElementById("body");
 
+      //       if(weatherImg.includes('clouds')){
+      //        res.write( 'bodyElement.style.backgroundColor="linear-gradient(135deg, #808080 0%, #363636 100%)";');
+      // }else if(weatherImg.includes('rain')){
+      //         weatherImg = 'rainy';
 
-//       if(weatherImg.includes('clouds')){
-//        res.write( 'bodyElement.style.backgroundColor="linear-gradient(135deg, #808080 0%, #363636 100%)";');
-// }else if(weatherImg.includes('rain')){
-//         weatherImg = 'rainy';
-        
-//         res.write('bodyElement.style.backgroundColor = "linear-gradient(135deg, #808080 0%, #363636 100%)";')
-//       }
-//       else if(weatherImg.includes('clear')){
-//         res.write('bodyElement.style.backgroundColor = "linear-gradient(135deg, #FFA500 0%, #FF4500 100%);";')
-//       }
-//       else if(weatherImg.includes('snow')){
-//         res.write('bodyElement.style.backgroundColor = "linear-gradient(135deg, #808080 0%, #363636 100%)";')
-//       }
+      //         res.write('bodyElement.style.backgroundColor = "linear-gradient(135deg, #808080 0%, #363636 100%)";')
+      //       }
+      //       else if(weatherImg.includes('clear')){
+      //         res.write('bodyElement.style.backgroundColor = "linear-gradient(135deg, #FFA500 0%, #FF4500 100%);";')
+      //       }
+      //       else if(weatherImg.includes('snow')){
+      //         res.write('bodyElement.style.backgroundColor = "linear-gradient(135deg, #808080 0%, #363636 100%)";')
+      //       }
 
       // Check weather City name exist or not
 
@@ -51,15 +53,11 @@ app.post('/', (req, res) => {
       //   res.write('<h1>City not found</h1>');
       //   res.send();
       // }
-     
-     // Handle error or city not found case
-     
 
-  
-            res.
-      write
-      (
-      `  
+      // Handle error or city not found case
+
+      res.write(
+        `  
       <head><meta charset="utf-8"><title>Weather App</title>
       <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -215,20 +213,14 @@ header>h1{
                 color: #fff;
               }
             `
-            
-
       );
-            res.
-      write
-      (
-      '</style>'
-      );
-    //   res.write(
-        // '<style>*{margin:0;padding:0}body{font-family:\'Open Sans\',sans-serif;display:flex;justify-content:center;align-items:center;height:100vh;margin:0;background:#302f2f;position:relative;background-image:url(./images/pexels-johannes-plenio-1119974.jpg);background-size:1300px 630px;background-position:center;background-repeat:no-repeat}.card{background-color:#000000b0!important;color:#fff!important;padding:2em;border:1px solid white;border-radius:28px;width:100%;max-width:420px;margin:1em}.search{display:flex;justify-content:center;align-items:center;margin:.5em}button{border:none;margin:.3em;background:#7c7c7c2b;color:#fff;height:2.6em;width:2.6em;border-radius:50%;font-size:1.2em;cursor:pointer;transition:.22s ease-in-out}button:hover{background:#595757}input.citySearched{border:none;outline:none;border-radius:11px;padding:.45em;color:#fff;background:#494444a9;width:calc(100%-100px);font-size:120%}</style>'
-    //   );
-      res.write('</head>');
+      res.write("</style>");
+      //   res.write(
+      // '<style>*{margin:0;padding:0}body{font-family:\'Open Sans\',sans-serif;display:flex;justify-content:center;align-items:center;height:100vh;margin:0;background:#302f2f;position:relative;background-image:url(./images/pexels-johannes-plenio-1119974.jpg);background-size:1300px 630px;background-position:center;background-repeat:no-repeat}.card{background-color:#000000b0!important;color:#fff!important;padding:2em;border:1px solid white;border-radius:28px;width:100%;max-width:420px;margin:1em}.search{display:flex;justify-content:center;align-items:center;margin:.5em}button{border:none;margin:.3em;background:#7c7c7c2b;color:#fff;height:2.6em;width:2.6em;border-radius:50%;font-size:1.2em;cursor:pointer;transition:.22s ease-in-out}button:hover{background:#595757}input.citySearched{border:none;outline:none;border-radius:11px;padding:.45em;color:#fff;background:#494444a9;width:calc(100%-100px);font-size:120%}</style>'
+      //   );
+      res.write("</head>");
       res.write('<body id="body">');
-      res.write('<header><h1>Weather App</h1></header> ');
+      res.write("<header><h1>Weather App</h1></header> ");
       res.write('<div class="card">');
       res.write('<form action="/" method="POST">');
       res.write('<div class="search">');
@@ -236,38 +228,46 @@ header>h1{
       res.write(
         '<input type="text" name="cityName" placeholder="Weather for city" class="citySearched">'
       );
-      res.write('<button type="submit" > <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.<style>svg{fill:#ffffff}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM288 176c0-44.2-35.8-80-80-80s-80 35.8-80 80c0 48.8 46.5 111.6 68.6 138.6c6 7.3 16.8 7.3 22.7 0c22.1-27 68.6-89.8 68.6-138.6zm-112 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg></button>');
-      res.write('</div>');
+      res.write(
+        '<button type="submit" > <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc.<style>svg{fill:#ffffff}</style><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM288 176c0-44.2-35.8-80-80-80s-80 35.8-80 80c0 48.8 46.5 111.6 68.6 138.6c6 7.3 16.8 7.3 22.7 0c22.1-27 68.6-89.8 68.6-138.6zm-112 0a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg></button>'
+      );
+      res.write("</div>");
       res.write(' <div class="weather-at">');
       res.write(
-          '<h2 h2 class="city"> Weather in ' +
+        '<h2 h2 class="city"> Weather in ' +
           CitySearched +
-          '<span id=country>'+countryName +'</span>'+
-          '</h2><div><h1 >' +
+          "<span id=country>" +
+          countryName +
+          "</span>" +
+          "</h2><div><h1 >" +
           temp +
-          ' &deg;C </h1>'
-          );
-          res.write('</div>');
-          res.write('<div class="gap">  ');
-          // res.write('<i class="fa-regular fa-clouds"></i>');
-          res.write(
-              '<div class="weather-description description"><h4>The weather is currently ' + weatherDescription + '</h4></div>'
-              );
-              res.write('<div class="humidity"><h4>Humidity: ' + Humidity + '%</h4></div>');
-      res.write('<div class="windSpeed"><h4>Wind Speed: ' + windSpeed + 'Km/Hour</h4></div>');
+          " &deg;C </h1>"
+      );
+      res.write("</div>");
+      res.write('<div class="gap">  ');
+      // res.write('<i class="fa-regular fa-clouds"></i>');
+      res.write(
+        '<div class="weather-description description"><h4>The weather is currently ' +
+          weatherDescription +
+          "</h4></div>"
+      );
+      res.write(
+        '<div class="humidity"><h4>Humidity: ' + Humidity + "%</h4></div>"
+      );
+      res.write(
+        '<div class="windSpeed"><h4>Wind Speed: ' +
+          windSpeed +
+          "Km/Hour</h4></div>"
+      );
 
-           res.write('</form>');
-      res.write('</div></body>');
-          
+      res.write("</form>");
+      res.write("</div></body>");
+
       res.send();
-
-          
     });
-  
   });
 });
 
 app.listen(3000, () => {
-
-  console.log('Server is running on port 3000');
+  console.log("Server is running on port 3000");
 });
